683a58488f0a9f46bb962bed4e4962ea52eb70d2
freeftr <jonghaparkdevelop@gmail.com>
2025-07-14 17:52:25 +0900

    chore: Initial commit

---
+++ src/main/java/com/market/market/common/exception/BadRequestException.java
@@ -0,0 +1,19 @@
+package com.market.market.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BadRequestException extends RuntimeException {
+    private final int code;
+    private final String message;
+
+    public BadRequestException(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    public BadRequestException(ErrorCode errorCode, String detail) P
+        this.code =errorCode.getCode();
+
+    }
+}
