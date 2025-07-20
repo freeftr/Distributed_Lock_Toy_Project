1b85f801622596359760b7b82dc0358d16a5a620
freeftr <jonghaparkdevelop@gmail.com>
2025-07-20 16:08:26 +0900

    feature: Define ShopProduct Domain

--- src/main/java/com/market/market/common/exception/ErrorCode.java
+++ src/main/java/com/market/market/common/exception/ErrorCode.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    INVALID_REQUEST(1000, "유효하지 않은 요청입니다.");
+    INVALID_REQUEST(1000, "유효하지 않은 요청입니다."),
+
+    PRODUCT_NOT_FOUND(2000, "존재하지 않는 상품입니다."),
+    PRODUCT_ALREADY_SOLD_OUT(2001, "이미 품절된 상품입니다."),
+    ;
 
     private final int code;
     private final String message;
