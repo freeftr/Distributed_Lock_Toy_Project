a0bec2597d47c5cdeccdf20ff7493b55dfb7e4c6
freeftr <jonghaparkdevelop@gmail.com>
2025-07-31 15:14:18 +0900

    chore: Add TransactionFacade

---
+++ src/main/java/com/market/market/common/tx/TransactionFacade.java
@@ -0,0 +1,14 @@
+package com.market.market.common.tx;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class TransactionFacade {
+
+	@Transactional
+	public Object proceed(final ProceedingJoinPoint joinPoint) throws Throwable {
+		return joinPoint.proceed();
+	}
+}
