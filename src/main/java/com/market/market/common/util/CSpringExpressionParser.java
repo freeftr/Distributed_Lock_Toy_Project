94ced3439359a4d5f90f5e51ca7512d614527543
freeftr <jonghaparkdevelop@gmail.com>
2025-07-31 15:05:40 +0900

    chore: Add CSpringExpressionParser

---
+++ src/main/java/com/market/market/common/util/CSpringExpressionParser.java
@@ -0,0 +1,22 @@
+package com.market.market.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CSpringExpressionParser {
+
+	public static Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
+		ExpressionParser parser = new SpelExpressionParser();
+		StandardEvaluationContext context = new StandardEvaluationContext();
+
+		for (int i = 0; i < parameterNames.length; i++) {
+			context.setVariable(parameterNames[i], args[i]);
+		}
+
+		return parser.parseExpression(key).getValue(context, Object.class);
+	}
+}
